ecadbdc8159d14f1b6c8e75c9e2a5c6f70c4571b
Gregor Srdic <gregor.srdic@gmail.com>
2017-07-16 18:49:07 +0200

    updated intro

--- contact.php
+++ contact.php
@@ -3,8 +3,8 @@
 /**
  * configure here
  */
-$from = 'IT WORKER <it@domain.com>';
-$sendTo = 'IT WORKER <it@domain.com>';
+$from = 'Portfolio Page <gregor.srdic@gmail.com>';
+$sendTo = 'Gregor Srdic <gregor.srdic@gmail.com>';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
 $htmlHeader = '';
